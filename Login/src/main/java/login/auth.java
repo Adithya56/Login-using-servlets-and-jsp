7c63816da969c3b329c289b3c7cff5f058baa1a7
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2023-05-22 06:53:19 +0530

    Add files via upload

---
+++ Login/src/main/java/login/auth.java
@@ -0,0 +1,33 @@
+package login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/auth")
+public class auth extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public auth() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		if(uname.equals("adi") && pass.equals("adi")) {
+			HttpSession hs=request.getSession();
+			hs.setAttribute("user",uname);
+			response.sendRedirect("welcome.jsp");
+		}
+		else {
+			response.sendRedirect("login.jsp");
+		}
+		
+		
+	}
+
+}
---
+++ Login/src/main/webapp/login.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="auth" method="get">
+	<table align="center">
+		<tr><th>Username: </th><td><input type="text" name="uname"></td></tr>
+		<tr><th>Password: </th><td><input type="password" name="pass"></td></tr>
+		<tr><td><input type="submit" id="submit" value="Login"></td></tr>
+	</table>
+</form>
+</body>
+</html>
